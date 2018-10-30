4bc9cf18e6aa4fc0514f580b87436114db2eb72c
Guillermo <guillermo@vitechd.com>
2018-10-29 19:34:22 -0600

    merge

---
+++ MascoticasTienda/Controllers/EfectivoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MascoticasTienda.Controllers
+{
+    public class EfectivoController : Controller
+    {
+        // GET: Efectivo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Reportes()
+        {
+            return View();
+        }
+        public ActionResult Cierre()
+        {
+            return View();
+        }
+        public ActionResult Movimientos()
+        {
+            return View();
+        }
+    }
+}
